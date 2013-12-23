cb19f78eb1205e3cd5a1494a0f7c8bf62e5afc73
Joooom <jonma752@student.liu.se>
2013-12-23 17:05:51 +0100

    tests and npmstuff

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
-var fs = require('fs')
-	,	path = require('path')
+var path = require('path')
 	,	spawn = require('child_process').spawn
 	,	guid = require('guid')
 
+var log = console.log
 // gpg --homedir d:/prog/nodejs/temppgp/tempdir/blah --import jon.pubkey
 
-function verify(pubkey, sig, done) {
+var errtext = "";
+
+function log_errs(spawn) {
+	spawn.stderr.on('data', function(err) {
+		errtext += err;
+	})
+}
+
+function verify(sig, pubkey, done) {
 	var rnd = guid.raw();
 	var homedir = path.resolve(__dirname, "tempdir", rnd)
 
 	var makedir = spawn("mkdir", [homedir])
+	//log(homedir)
+	log_errs(makedir)
 	makedir.on('close', function(code) {
 		var makering = spawn("gpg", ["--homedir", homedir, "--import"])
 		makering.stdin.write(pubkey)
 		makering.stdin.end()
-
+		
+		//log_errs(makering)
 		makering.on('close', function(code) {
 			
 			var verify = spawn("gpg", ["--homedir", homedir, "--verify"])
 			verify.stdin.write(sig)
 			verify.stdin.end()
 			
+			//log_errs(verify)
 			verify.on('close', function(code) {
-				log("code from verify: ", code)
+				//console.log("code from verify: ", code)
 				
-				var success = code === 0;
+				var success = code === 0
 				// ok we're done, lets remove the temp folder.
 				spawn("rm", ["-r", homedir])
 				
-				done(null, success)
+				if (errtext) {
+					done(errtext, false)
+				} else {
+					done(null, success)
+				}
 			})
 		})
 	})
--- test.js
+++ test.js
@@ -1,17 +1,37 @@
+var fs = require('fs')
+	,	gpgverify = require('./app')
+	,	assert = require("assert")
 
-var gpgverify = require('./app')
-
-
+var log = console.log
 var opts = { encoding: "ascii" }
 var sig = fs.readFileSync("challenge.sig", opts)
 var pubkey = fs.readFileSync("jon.pubkey", opts)
 var pubkey2 = fs.readFileSync("jon.m2.pubkey", opts)
 
 
-gpgverify(sig, pubkey, function(err, status) {
-	console.log("first test: " + status === true)
+describe("gpg-verify", function() {
+	it("should return true for a signature with matching pubkey", function(done) {
+		gpgverify(sig, pubkey, function(err, status) {
+			if (err) throw err;
+			assert.equal(status, true)
+			done();
+		})	
+	})
+
+	it("should return false for a signature with a non-matching pubkey", function(done) {
+		gpgverify(sig, pubkey2, function(err, status) {
+			if (err) throw err;
+			assert.equal(status, false);
+			done();
+		})
+	})
+
+	it("should return false for a broken signature", function(done) {
+		gpgverify("kek", pubkey2, function(err, status) {
+			if (err) throw err;
+			assert.equal(status, false);
+			done();
+		})
+	})
 })
 
-gpgverify(sig, pubkey2, function(err, status) {
-	console.log("second test: " + status === false)
-})
